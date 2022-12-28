a06f0904f2ab68ba979426e6f8a2ed598562e779
unknown <christopher.perry410@gmail.com>
2022-12-28 12:30:32 -0500

    Wezterm: update binds and notate config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,72 +1,103 @@
-local wezterm = require 'wezterm'    -- needed for font function
-local mux = wezterm.mux
+--[[
+  ****************
+    Imports & Misc
+  ****************
+]]
+local wezterm = require 'wezterm'
 
-wezterm.on("gui-startup", function()
+-- Always start maximized
+local mux = wezterm.mux
+wezterm.on (
+  "gui-startup", function()
   local tab, pane, window = mux.spawn_window{}
   window:gui_window():maximize()
-end)
+  end )
 
 return {
-  
-  skip_close_confirmation_for_processes_named = {nil},
-  animation_fps = 30,
-  exit_behavior = "Hold",
-  default_cursor_style = "SteadyBlock",
-  
-  -- Default Shell
-  default_prog = {"pwsh.exe", "-NoLogo"},
-  
-  -- ColorSchemes and font settings
+  --[[
+  ****************
+    Appearance
+  ****************
+  ]]
   color_scheme = "Catppuccin Frappe",
-  font = wezterm.font("Source Code Pro", {weight = "DemiBold"}),
+  font = wezterm.font( 
+    "Source Code Pro", { weight = "DemiBold" }
+  ),
+  default_cursor_style = "SteadyBlock",
+  enable_tab_bar = true,
+  use_fancy_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
+  tab_bar_at_bottom = true,
+  animation_fps = 30,
   
-  
-  -- window_padding = {
-    -- left   = "1px",
-    -- right  = "1px",
-    -- top    = "1px",
-    -- bottom = "1px",
-  -- },
-  
-  enable_tab_bar = false,
-  
-  -- Leader Key and Leader + <Key> binds
-  leader = { key = ' ', mods = 'SHIFT'},
+  --[[
+  ****************
+    Behavior
+  ****************
+  ]]
+  default_prog = { "pwsh.exe", "-NoLogo" },
+  skip_close_confirmation_for_processes_named = { nil },
+  exit_behavior = "Hold",
 
-   keys = {
-    { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Next' },
-    
-    { key = 'v', mods = 'LEADER', action = wezterm.action.SplitPane {
-      direction = "Right",
-      size = { Percent = 35 },
-      },
+  --[[ 
+  ****************
+    KEYBINDS
+  ****************
+  ]]   
+  leader = { key = ' ', mods = 'SHIFT' },
+  keys = {
+    { 
+      -- Activate Next Pane
+      key = 'j', 
+      mods = 'LEADER', 
+      action = wezterm.action.ActivatePaneDirection 'Next' 
     },
-    
-    { key = '\\', mods = 'LEADER', action = wezterm.action.CloseCurrentPane { confirm = true } },
-    
-   
-    { key = "'", mods = 'LEADER', action = wezterm.action.SpawnCommandInNewTab},
-    { key = 'k', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1)},
-    
-    -- ShowLauncher binds
-  
-    -- Show default launcher
-    { key = '[', mods = 'LEADER', action = wezterm.action.ShowLauncher },
-    
-    -- Show w/ Keybinds
-    { key = 't', mods = 'LEADER', action = wezterm.action.ShowLauncherArgs {
-      flags = 'FUZZY|KEY_ASSIGNMENTS'
-    }},
-    
-    -- ShowLauncher w/ Workspaces
-    { key = 'w', mods = 'LEADER', action = wezterm.action.ShowLauncherArgs { 
-      flags = 'FUZZY|WORKSPACES'
-    }},
-    
-    -- ShowLauncher w/ tabs
-    { key = 't', mods = 'LEADER', action = wezterm.action.ShowLauncherArgs {
-      flags = 'FUZZY|TABS'
+    { 
+      -- Split Pane Vertically
+      key = 'v',
+      mods = 'LEADER',
+      action = wezterm.action.SplitPane {
+        direction = "Right",
+        size = { Percent = 35 }}
+    },
+    { 
+      -- Close Pane w/ Confirmation
+      key = '\\',
+      mods = 'LEADER',
+      action = wezterm.action.CloseCurrentPane { 
+        confirm = true }
+    },
+    { 
+      -- New Tab
+      key = "'",
+      mods = 'LEADER',
+      action = wezterm.action.SpawnCommandInNewTab
+    },
+    { 
+      -- Activate Next Tab
+      key = 'k', 
+      mods = 'LEADER', 
+      action = wezterm.action.ActivateTabRelative(1)
+    },
+    { 
+      -- ShowLauncher
+      key = '[',
+      mods = 'LEADER', 
+      action = wezterm.action.ShowLauncherArgs { 
+        flags = 'FUZZY' }
+    },
+    { 
+      -- ShowLauncher w/ Workspaces
+      key = 'w', 
+      mods = 'LEADER', 
+      action = wezterm.action.ShowLauncherArgs { 
+        flags = 'FUZZY|WORKSPACES|TABS' }
+    },
+    { 
+      -- ShowLauncher (Tabs)
+      key = 't',
+      mods = 'LEADER',
+      action = wezterm.action.ShowLauncherArgs {
+        flags = 'FUZZY|TABS' }
     }}
-  }
-  
 } 
