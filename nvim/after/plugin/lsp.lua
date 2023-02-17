09acb7d53a1ea6311774604c16c73130fb69105b
Christopher Perry <christopher.perry410@gmail.com>
2023-02-17 15:10:10 -0500

    Nvim: lsp floating windows are not ugly anymore

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,43 +3,41 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-k>'] = cmp.mapping.scroll_docs( -4),
+    ['<C-j>'] = cmp.mapping.scroll_docs(4),
 })
 
+lsp.setup_nvim_cmp({ mapping = cmp_mappings, })
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
+lsp.set_preferences({ suggest_lsp_servers = false, })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-  -- showing: [l]anguage [s]howing
-  -- go to: [l]anguage [g]o
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "<leader>d", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("n", "<leader>F", function() vim.lsp.buf.format { async = true } end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>d", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, opts)
+
+    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("v", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+
+    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>F", function() vim.lsp.buf.format { async = true } end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover, {
+    border = nil,
+    title = nil,
+})
 
 vim.diagnostic.config({
     virtual_text = false,
@@ -53,6 +51,5 @@ vim.diagnostic.config({
         source = 'always',
         header = '',
         prefix = '',
-  },
+    },
 })
-
