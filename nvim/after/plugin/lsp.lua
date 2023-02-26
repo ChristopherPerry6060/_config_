a89e2a5feab76dcd38f60a16aa571b3f63aa3d99
Chris Perry <christopher.perry410@gmail.com>
2023-02-26 01:16:39 -0500

    Nvim: update lsp settings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,6 +1,8 @@
 local lsp = require("lsp-zero")
-
-lsp.preset("recommended")
+lsp.preset({ 
+    name = "recommended", 
+    suggest_lsp_servers = false,
+})
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -15,18 +17,14 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 
 lsp.setup_nvim_cmp({ mapping = cmp_mappings, })
 
-lsp.set_preferences({ suggest_lsp_servers = false, })
-
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "<leader>D", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>l", function() vim.diagnostic.open_float() end, opts)
-
-    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("v", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
-
+    vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.format { async = true } end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
