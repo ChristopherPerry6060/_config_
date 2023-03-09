8f881dc8d3c2ae667da006afa302cc05cd2bcfae
Chris Perry <christopher.perry410@gmail.com>
2023-03-08 23:16:08 -0500

    Nvim: harpoon

--- nvim/after/plugin/everything.lua
+++ nvim/after/plugin/everything.lua
@@ -25,6 +25,8 @@ lsp.setup_nvim_cmp({
     }
 })
 
+lsp.setup()
+
 lsp.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "<leader>D", function() vim.lsp.buf.definition() end, opts)
@@ -37,7 +39,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-lsp.setup()
+
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     vim.lsp.handlers.hover, {
@@ -64,18 +66,19 @@ vim.diagnostic.config({
 local builtin = require('telescope.builtin')
 require("telescope").load_extension('harpoon')
 
+
 vim.keymap.set('n', '<leader>f', builtin.find_files, {}) -- Find File
-vim.keymap.set('n', '<leader>j', builtin.buffers, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>x', builtin.quickfix, {})
 vim.keymap.set('n', '<leader>e', builtin.registers, {})
-
-vim.keymap.set('n', '<leader>d', builtin.diagnostics, {}) -- Find Diagnostics
 vim.keymap.set('n', '<leader>R', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>i', builtin.lsp_implementations, {})
 vim.keymap.set('n', '<leader>t', builtin.lsp_type_definitions, {})
 vim.keymap.set('n', '<leader>S', builtin.lsp_workspace_symbols, {}) -- Find Workspace Symbols
 vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols, {}) -- Find Workspace Symbols
+-- Find Diagnostics
+vim.keymap.set('n', '<leader>d', function() builtin.diagnostics({ line_width = 1000, no_sign = true }) end, {})
+
 
 -- Harpoon Harpoon
 -- Harpoon Harpoon
@@ -84,17 +87,25 @@ local harpoon_mark = require("harpoon.mark")
 local harpoon = require("harpoon")
 
 vim.keymap.set('n', '<leader>M', ':Telescope harpoon marks<CR>', {})
-vim.keymap.set('n', '<leader>m', harpoon_ui.toggle_quick_menu, {}) 
-vim.keymap.set('n', '<leader>n', harpoon_mark.add_file, {}) 
+vim.keymap.set('n', '<leader>j', harpoon_ui.toggle_quick_menu, {})
+vim.keymap.set('n', '<leader>J', harpoon_mark.add_file, {})
 vim.keymap.set('n', '<leader>1', function() harpoon_ui.nav_file(1) end, {})
 vim.keymap.set('n', '<leader>2', function() harpoon_ui.nav_file(2) end, {})
 vim.keymap.set('n', '<leader>3', function() harpoon_ui.nav_file(3) end, {})
 vim.keymap.set('n', '<leader>4', function() harpoon_ui.nav_file(4) end, {})
+require('telescope').setup {
+    defaults = {
+        layout_strategy = 'center',
+        layout_config = { width = 0.95, height = 0.95, prompt_position = 'bottom' },
+        wrap_results = false,
+        border = false,
+    },
+}
 
 harpoon.setup({
     global_settings = {
         -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-        save_on_toggle = false,
+        save_on_toggle = true,
         -- saves the harpoon file upon every change. disabling is unrecommended.
         save_on_change = true,
         -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
@@ -106,26 +117,25 @@ harpoon.setup({
 })
 
 
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "rust" },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = { "rust" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = false,
 
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
-
--- nvim/init.lua
+++ nvim/init.lua
@@ -6,41 +6,41 @@ require('shared.set')
 
 return require('packer').startup(function(use)
     -- Lua
-    use ('kdheepak/lazygit.nvim')
-    use ('wbthomason/packer.nvim')
-    use ('Canop/nvim-bacon')
-    use ('tpope/vim-surround')
-    use ('ThePrimeagen/harpoon')
-    use ('lukas-reineke/indent-blankline.nvim')
+    use('kdheepak/lazygit.nvim')
+    use('wbthomason/packer.nvim')
+    use('Canop/nvim-bacon')
+    use('tpope/vim-surround')
+    use('ThePrimeagen/harpoon')
+    use('lukas-reineke/indent-blankline.nvim')
     use('nvim-treesitter/playground')
+    use('vimwiki/vimwiki')
+
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use { 'catppuccin/nvim', as = 'catppuccin' }
-
     -- -- Telescope related
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.1',
-       requires = { {'nvim-lua/plenary.nvim'} }
-     }
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
     use { 'nvim-telescope/telescope-file-browser.nvim' }
 
     -- -- LSP
     use {
-      'VonHeikemen/lsp-zero.nvim',
-      requires = {
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},
-          {'williamboman/mason.nvim'},
-          {'williamboman/mason-lspconfig.nvim'},
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},
-          {'hrsh7th/cmp-buffer'},
-          {'hrsh7th/cmp-path'},
-          {'saadparwaiz1/cmp_luasnip'},
-          {'hrsh7th/cmp-nvim-lsp'},
-          {'hrsh7th/cmp-nvim-lua'},
-          -- Snippets
-          {'L3MON4D3/LuaSnip'},
-          {'rafamadriz/friendly-snippets'},
-      }
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
     }
-
 end)
